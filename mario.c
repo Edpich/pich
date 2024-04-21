22c338ff1f51193d3bfe7d19e2e068e963f363b7
Edpich <166917945+Edpich@users.noreply.github.com>
2024-04-21 21:50:26 +0000

    Sun, Apr 21, 2024, 6:50 PM -03:00

---
+++ mario.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int altura, i, j;
+
+    // Pede ao usuário para digitar a altura da pirâmide
+    printf("Digite a altura da pirâmide: ");
+    scanf("%d", &altura);
+
+    // Loop para as linhas da pirâmide
+    for (i = 1; i <= altura; i++) {
+        // Imprime espaços à esquerda
+        for (j = 1; j <= altura - i; j++) {
+            printf("  ");
+        }
+        // Imprime os hashes
+        for (j = 1; j <= i; j++) {
+            printf("#");
+        }
+        // Move para a próxima linha
+        printf("\n");
+    }
+
+    return 0;
+}
